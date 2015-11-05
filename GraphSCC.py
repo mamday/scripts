f5d9bc91db7af7807345fd5d70bff2c6b3c2c2a7
mamday <m.amday@gmail.com>
2015-11-05 13:32:30 -0600

    Definitely finds SCCs in some cases. Need to deal with edge case where some nodes do not lead to other nodes

--- GraphSCC.py
+++ GraphSCC.py
@@ -27,39 +27,56 @@ for line in open(file_name).readlines():
   graphrev_nodes[int(in_list[1])][-1].append(0)
   graphrev_nodes[int(in_list[1])][-1].append(False)
 
-print graphrev_nodes,graph_nodes
-global all_leader,leader,count
+#print graphrev_nodes,graph_nodes
+global sccs,all_leader,leader,count
 all_leader = []
-
-def ExpNode(nodes,g_nodes):
-  global count,all_leader,leader
+sccs = {key: [] for key in graph_nodes.keys()}
+def ExpNode(nodes,g_nodes,lead):
+  global count,sccs,all_leader,leader
   for node in nodes:
     #print 'Bef',leader,count,node
     if(node[3]==False):
       node[3]=True
-      #print node[0],node[1]
-      if(False in [i[3] for i in g_nodes[node[1]]]):
-        ExpNode(g_nodes[node[1]],g_nodes)
+      if(node[1] in g_nodes.keys()):
+        print g_nodes[node[1]] 
+        if(False in [i[3] for i in g_nodes[node[1]]]):
+          ExpNode(g_nodes[node[1]],g_nodes,lead)
+      else:
+        continue
     else:
       continue
     leader[node[0]]+=1
     if(leader[node[0]]>=len(g_nodes[node[0]])): 
       count+=1
       node[2]=count
-      all_leader.append(node[0])
-      #print 'Aft',count,node    
+      if(not(lead>-1)):
+        all_leader.append(node[0])
+      else:
+        sccs[lead].append(node[0])
+      #print 'Aft',sccs,count,node    
 
 def FindLeaders(g_nodes):
-  global leader,count,all_leader
-  graph_list = []
+  global leader,count
   count = 0
   leader = {key: 0 for key in g_nodes.keys()} 
+  lead = -1 
   for ind,nodes in g_nodes.iteritems():
-    print ind,nodes
-    print ind,nodes
-    #if(len(all_leader)<1 and ind!=8):
-    #  continue
-    ExpNode(nodes,g_nodes)
-  return all_leader
-leaders = FindLeaders(graphrev_nodes)
-print graphrev_nodes,graph_nodes
+    ExpNode(nodes,g_nodes,lead)
+  
+FindLeaders(graphrev_nodes)
+
+def FindSCC(g_nodes):
+  global leader,count,all_leader
+  count = 0
+  leader = {key: 0 for key in g_nodes.keys()}
+  all_leader.reverse()
+  for head in all_leader:
+    lead=head
+    ExpNode(g_nodes[head],g_nodes,lead)
+    #print head
+
+FindSCC(graph_nodes)
+#print graphrev_nodes,graph_nodes
+print 'SCCs:'
+for ind,scc in sccs.iteritems():
+  print ind,scc,len(scc)
