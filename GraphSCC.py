2c27b1061d8cf93212dec32afef78ece2977b7f8
mamday <m.amday@gmail.com>
2015-11-11 13:47:38 -0600

    Improve inputs and some speed things

--- GraphSCC.py
+++ GraphSCC.py
@@ -8,33 +8,33 @@ from copy import deepcopy
 file_name = sys.argv[1]
 global graph_nodes
 global graphrev_nodes
-graph_nodes = {key: [] for key in xrange(0,1000000)} 
-graphrev_nodes = {key: [] for key in xrange(0,1000000)}
+#Make obnoxiously large dictionaries to avoid having to search through keys
+graph_nodes = {key: [] for key in [str(i) for i in xrange(0,1000000)]} 
+graphrev_nodes = {key: [] for key in [str(i) for i in xrange(0,1000000)]}
  
 for line in open(file_name).readlines():
   in_list = line.split(' ')
+  sec_list = line.split(' ')
   in_list = in_list[:2]
-#  try:
-#    for i in in_list:
-#      if(i!='\n'):
-#        int(i)
-#  except:
-#    print 'Failed',in_list,line
-  num_list = [int(i) for i in in_list if(i!=False and (i.isdigit() or not(i.isspace())))]
-  graph_nodes[int(in_list[0])].append(deepcopy(num_list))
-  graph_nodes[int(in_list[0])][-1].append(0)
-  graph_nodes[int(in_list[0])][-1].append(False)
+  sec_list = sec_list[:2]
 
-  num_list.reverse()
-  graphrev_nodes[int(in_list[1])].append(deepcopy(num_list))
-  graphrev_nodes[int(in_list[1])][-1].append(0)
-  graphrev_nodes[int(in_list[1])][-1].append(False)
+  num_list = in_list
+  graph_nodes[num_list[0]].append(num_list)
+  graph_nodes[num_list[0]][-1].append(0)
+  graph_nodes[num_list[0]][-1].append(False)
+
+  sec_list.reverse()
+  num_list1 = sec_list
+  graphrev_nodes[num_list1[0]].append(num_list1)
+  graphrev_nodes[num_list1[0]][-1].append(0)
+  graphrev_nodes[num_list1[0]][-1].append(False)
 
 print 'Loaded'
 #print graphrev_nodes,graph_nodes
 global sccs,all_leader,leader,count
 all_leader = []
-sccs = {key: [] for key in xrange(1+max([max(graph_nodes.keys()),max(graphrev_nodes.keys())]))}
+#sccs = {key: [] for key in xrange(1+max([max(graph_nodes.keys()),max(graphrev_nodes.keys())]))}
+sccs = {key: [] for key in [str(i) for i in xrange(0,1000000)]} 
 #Today I learned that recursion is not always a good idea
 def RecExpNode(nodes,g_nodes,lead):
   global count,sccs,all_leader,leader
@@ -65,76 +65,119 @@ def RecExpNode(nodes,g_nodes,lead):
 
 def ExpNode(nodes,g_nodes,lead):
     global count,sccs,all_leader,leader
-    in_tails = []
     cur_heads = [] 
     e_bool = True
+    #if(len(nodes)<1):
+    #  return 0 
+    #print 'Begin Awesome'
     cur_node = nodes[-1][0]
     while(e_bool):
-      print cur_node,count,cur_heads,in_tails
+      #print cur_node,len(g_nodes[cur_node]),e_bool
+      #print 'Beg',e_bool,cur_node,cur_heads,all_leader
       if(len(g_nodes[cur_node])>leader[cur_node]):
+        #print 'Explore Lead'
         exp_node = g_nodes[cur_node][leader[cur_node]]
-        print 'Explore',exp_node
+        #print 'Explore',exp_node
         leader[cur_node]+=1
         if(exp_node[3]==False):
-          print 'Here'
+          #print 'Explore False'
           exp_node[3]=True
           if(leader[cur_node]==1):
             cur_heads.append(cur_node)
           if(len(g_nodes[exp_node[1]])>0):
             cur_node = exp_node[1]
           else:
+            #print 'Explore Edge'
             count+=1
-            g_nodes[-1][2]=count
-            in_tails.append(exp_node[1])
+            g_nodes[cur_node][-1][2]=count
+            if(not(lead>-1)):
+              all_leader.append(exp_node[1])
             cur_node = cur_heads[-1]
-        #else:
-        #  count+=1
-        #  g_nodes[cur_node][-1][2]=count
-        #  in_tails.append(cur_node)
-        #  cur_heads = cur_heads[:-1]
-        #  cur_node = cur_heads[-1]
+        else:
+          #print 'Explore True',cur_node,cur_heads
+          if(cur_node!=nodes[-1][0]):
+            if(len(g_nodes[cur_heads[-1]])==leader[cur_heads[-1]]):
+              if(len(cur_heads)>1):
+                count+=1
+                g_nodes[cur_node][-1][2]=count
+                if(not(lead>-1)):
+                  all_leader.append(cur_heads[-1])
+                else:
+                  cur_node = cur_heads[-1]
+              else:
+                if(not(lead>-1)):
+                  all_leader.append(cur_heads[-1])
+                else:
+                  sccs[lead].append(cur_heads[-1])
+                  #print 'SCC',lead,sccs[lead]
+                e_bool = False
+            else:
+              cur_node = cur_heads[-1]
+          else:
+            e_bool = False
       else:
         if(len(cur_heads)>1):
-          print 'Explore End',cur_node,exp_node
-          next_node = g_nodes[cur_node][leader[cur_node]-1][1]
+          #next_node = g_nodes[cur_node][leader[cur_node]-1][1]
           #if(not(leader[next_node]<len(g_nodes[next_node]))):
-          count+=1
-          g_nodes[cur_node][-1][2]=count
-          in_tails.append(cur_heads[-1])
-          cur_heads = cur_heads[:-1]
-          cur_node = cur_heads[-1]
+          if(len(g_nodes[cur_heads[-1]])==leader[cur_heads[-1]]):
+            count+=1
+            g_nodes[cur_node][-1][2]=count
+            if(not(lead>-1)):
+              all_leader.append(cur_heads[-1])
+            else:
+              sccs[lead].append(cur_heads[-1])
+              #print 'SCC',lead,sccs[lead]
+            cur_heads = cur_heads[:-1]
+            cur_node = cur_heads[-1]
+          else:
+            cur_node = cur_heads[-1]
         else:
-          e_bool = False
+          if(len(g_nodes[cur_heads[0]])==leader[cur_heads[0]]):
+            #print 'Explore Full Stop'
+            if(not(lead>-1)):
+              all_leader.append(cur_heads[0])
+            else:
+              sccs[lead].append(cur_heads[0])
+              #print 'SCC',lead,sccs[lead]
+            e_bool = False
+          else:
+            cur_node = cur_heads[0]
+      #print e_bool,cur_node,cur_heads,all_leader
 
 def FindLeaders(g_nodes):
   global leader,count
-  count = 0
-  leader = {key: 0 for key in g_nodes.keys()} 
-  lead = -1 
-#  for ind,nodes in g_nodes.iteritems():
-  nodes = g_nodes[1] 
-  ExpNode(nodes,g_nodes,lead)
+  for ind,nodes in g_nodes.iteritems():
+    if(len(nodes)==0 or not(False in [i[3] for i in nodes])):
+      continue
+    count = 0
+    lead = -1 
+    leader = {key: 0 for key in g_nodes.keys()} 
+    ExpNode(nodes,g_nodes,lead)
   
 FindLeaders(graphrev_nodes)
 print 'Found Leaders'
 
-#def FindSCC(g_nodes):
-#  global leader,count,all_leader
-#  count = 0
-#  leader = {key: 0 for key in g_nodes.keys()}
-#  all_leader.reverse()
-#  for head in all_leader:
-#    lead=head
-#    if(not(len(g_nodes[head])>0)):
-#      sccs[head].append(head)
-#    else:
-#      ExpNode(g_nodes[head],g_nodes,lead)
+def FindSCC(g_nodes):
+  global leader,count,all_leader
+  all_leader.reverse()
+  for head in all_leader:
+    #print head
+    count = 0
+    lead=head
+    leader = {key: 0 for key in g_nodes.keys()}
+    if(not(False in [i[3] for i in g_nodes[head]])):
+      if(not(len(g_nodes[head])>0)):
+        sccs[head].append(head)
+      continue
+    ExpNode(g_nodes[head],g_nodes,lead)
 
-#FindSCC(graph_nodes)
+FindSCC(graph_nodes)
 
 #print graphrev_nodes,graph_nodes
 
-#print 'SCCs:'
-#for ind,scc in sccs.iteritems():
-#  if(len(scc)>0):
-#    print ind,scc,len(scc)
+
+print 'SCCs:'
+for ind,scc in sccs.iteritems():
+  if(len(scc)>0):
+    print ind,scc,len(scc)
+
