ab535a2f5ad1f3eb2e93f87a37d15c97a2364d59
mamday <m.amday@gmail.com>
2015-12-01 16:02:59 -0600

    Advent of Code: Day1 part 2

--- parenmasher.py
+++ parenmasher.py
@@ -8,9 +8,14 @@ santa_text = ''
 for line in open(in_file).readlines():
   santa_text = line
 count=0
+count1=0
 for i in santa_text:
+  count1+=1
   if(i=='('):
     count+=1  
   if(i==')'):
     count-=1  
+  if(count<0):
+    print count1
+    break
 print count  
