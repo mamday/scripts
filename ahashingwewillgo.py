cd4dbfda8168c3b3c77a66daf08dfa8d403b8b39
mamday <m.amday@gmail.com>
2016-01-05 15:39:21 -0600

    Advent of Code: Day4

--- ahashingwewillgo.py
+++ ahashingwewillgo.py
@@ -6,6 +6,7 @@ from multiprocessing import Process, Lock, Queue
 my_string = sys.argv[1]
 cur_hash = ''
 
+#Just mines bitcoins or whatever
 def HashingThroughTheSnow(beg_int,end_int):
   for my_ints in xrange(beg_int,end_int):
     cur_hash = hashlib.md5(my_string+str(my_ints)).hexdigest()
