d1ea3b768e282e302f7756dd85c9b4150ad44c55
mamday <m.amday@gmail.com>
2016-01-07 11:22:02 -0600

    Advent of Code: Day20

--- houses.py
+++ houses.py
@@ -1,20 +1,22 @@
 import sys,os
 
 in_num = int(sys.argv[1])
-in_elves = int(sys.argv[2])
+#in_elves = int(sys.argv[2])
 house_dict = {}
-for i in xrange(1,in_elves):
-  for j in xrange(0,in_elves,i):
+for i in xrange(1,(in_num/10)+1):
+  house_dict[i]=0
+
+for j in xrange(1,in_num/10):
+  print j 
+  for i in xrange(j,in_num/10,j):
     #print [True for key,val in house_dict.iteritems() if(val>in_num)]
-    if(not(True in [True for key,val in house_dict.iteritems() if val>in_num])):
-      if(j==0):
+    #print 'Test',house_dict
+    #if(not(True in [True for key,val in house_dict.iteritems() if val>=in_num])):
+      if(i==0):
         continue
-      if(j in house_dict):
-        #print i,j,house_dict[j],j*10
-        house_dict[j]+=i*10
-      else:
-        #print i,j,j*10
-        house_dict[j]=i*10
-    else:
-       break
-print house_dict,[key for key,val in house_dict.iteritems() if(val>in_num)]
+      house_dict[i]+=j*10
+      #print j,i,j*10
+    #else:
+    #  break
+#print house_dict,[key for key,val in house_dict.iteritems() if(val>=in_num)]
+print [key for key,val in house_dict.iteritems() if(val>=in_num)]
