017dac7b22319a4ab77a8e0bb6ef897f9731d22a
mamday <m.amday@gmail.com>
2016-01-07 10:07:35 -0600

    Fast but does not give the right answer. Sigh. Work in Progress

---
+++ houses1.py
@@ -0,0 +1,21 @@
+import sys,os
+
+in_num = int(sys.argv[1])
+#in_elves = int(sys.argv[2])
+house_dict = {}
+for i in xrange(1,(in_num/10)+1):
+  house_dict[i]=0
+
+for i in xrange(j,in_num/10,j):
+  for j in xrange(1,in_num/10):
+  print j
+  #print [True for key,val in house_dict.iteritems() if(val>in_num)]
+  #print 'Test',house_dict
+  if(not(True in [True for key,val in house_dict.iteritems() if val>=in_num])):
+      if(i==0):
+        continue
+      house_dict[i]+=j*10
+      #print j,tot,j*10
+  else:
+     break
+print house_dict,[key for key,val in house_dict.iteritems() if(val>=in_num)]
