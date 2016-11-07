4f928ebd505a91fbe69689571c9f706b26356c97
Melanie Day <mamday@gmail.com>
2016-11-07 13:07:43 -0800

    Create a new class that inherits from fizz_rand_list. Overload the constructor so that the inherited class only ever uses the constructor that picks numbers between 1-1000

--- get_rand_stats.cxx
+++ get_rand_stats.cxx
@@ -3,11 +3,31 @@
 #include <stdlib.h>
 #include "get_rand_stats.h"
 
+int_rnd_list::int_rnd_list(int N):fizz_rnd_list(N){
+
+}
+
+int_rnd_list::~int_rnd_list(){
+
+}
+
+fizz_rnd_list::fizz_rnd_list(float N){
+  int int_N = static_cast<int>(N);
+  random_list__ = new double[int_N]();
+  srand((unsigned int) time(NULL));
+  for(int i=0; i<int_N; ++i){
+    random_list__[i]=((float)rand()/(float)(RAND_MAX));
+  }
+  for(int i=0; i<int_N; ++i){
+    std::cout<<random_list__[i]<<std::endl;
+  }
+}
+
 fizz_rnd_list::fizz_rnd_list(int N){
   random_list__ = new double[N]();
   srand((unsigned int) time(NULL));
   for(int i=0; i<N; ++i){
-    random_list__[i]=((float)rand()/(float)(RAND_MAX));
+    random_list__[i]=rand() % 1000;
   }
   for(int i=0; i<N; ++i){
     std::cout<<random_list__[i]<<std::endl;
@@ -19,6 +39,9 @@ fizz_rnd_list::~fizz_rnd_list(){
 }
 
 int main(){
-  fizz_rnd_list rnd(10);
+  float num = 10.0;
+  int i_num = 10;
+  fizz_rnd_list f_rnd(num);
+  int_rnd_list i_rnd(num);
 }
 
--- get_rand_stats.h
+++ get_rand_stats.h
@@ -6,8 +6,17 @@
 class fizz_rnd_list{
   public:
     fizz_rnd_list(int N);
+    fizz_rnd_list(float N);
     virtual ~fizz_rnd_list();
     std::tuple<double, int, double, double> get_stats(double X);
+    std::tuple<int, int, double, double> get_stats(int X);
   private:
     double* random_list__;
+};
+
+class int_rnd_list: public fizz_rnd_list{
+  public:
+    int_rnd_list(int N);
+    ~int_rnd_list();
+
 };
