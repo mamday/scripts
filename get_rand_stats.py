4ed8ce62363d3be6354060109f252b98a1ff4ebf
Melanie Day <mamday@gmail.com>
2016-11-09 12:16:06 -0800

    Tested implementing a method for calculating and storing std dev and see that the numpy method is about twice as fast.

--- get_rand_stats.py
+++ get_rand_stats.py
@@ -11,7 +11,7 @@ class fizz_rnd_list(object):
     self.rand_list.sort()
     self.mean_dict = {0:0}
     self.std_dev_dict = {0:0}
-    self.max_dict_ind = 0 
+    self.max_mean_dict_ind = 0 
   def __str__(self):
     '''Print a list of the numbers in self.rand_list'''
     out_str = '['
@@ -21,6 +21,7 @@ class fizz_rnd_list(object):
       out_str = out_str[:-2]
     out_str+=']'
     return out_str 
+
   '''This version of calculating the mean saves the result if an index has
   already been calculated. If the index is greater than the maximum index
   that was ever calculated, it starts from the maximum index value and only
@@ -33,17 +34,34 @@ class fizz_rnd_list(object):
     if(ind in self.mean_dict):
       return self.mean_dict[ind]
 
-    if(ind>self.max_dict_ind):
-      cur_tot = (self.max_dict_ind)*self.mean_dict[self.max_dict_ind] 
+    if(ind>self.max_mean_dict_ind):
+      cur_tot = (self.max_mean_dict_ind)*self.mean_dict[self.max_mean_dict_ind] 
 
-    for i in xrange(self.max_dict_ind,ind):
+    for i in xrange(self.max_mean_dict_ind,ind):
        cur_tot+=self.rand_list[i]
        self.mean_dict[i+1]=cur_tot/(i+1)
 
-    if((ind)>self.max_dict_ind):
-      self.max_dict_ind=ind
+    if((ind)>self.max_mean_dict_ind):
+      self.max_mean_dict_ind=ind
 
     return cur_tot/(ind)
+
+  '''This version of calculating the standard deviation saves the result if an 
+  index has already been calculated, similar to the implementation for 
+  get_mean(). However, since the mean of the distribution will be different for
+  different indices, it does not save the results as it calculates them.'''
+  def get_std_dev(self,ind):
+    if(not(ind in self.mean_dict)):
+      raise Exception('Must calculate mean before calculating standard deviation')
+    cur_tot=0.0
+    if(ind in self.std_dev_dict):
+      return self.std_dev_dict[ind]
+    var_array = (numpy.array(self.rand_list[:ind])-self.mean_dict[ind])*(numpy.array(self.rand_list[:ind])-self.mean_dict[ind])
+    cur_tot= sum(var_array)
+
+    self.std_dev_dict[ind]=numpy.sqrt(cur_tot/(ind))
+
+    return self.std_dev_dict[ind] 
 
   def get_stats(self,X):
     '''Use a binary tree to find the input value in the sorted list. 
@@ -72,20 +90,24 @@ class fizz_rnd_list(object):
         r_nums = high_nums
       if(len(r_nums)==1):
         close_val=r_nums[0]
-    '''A way of calculating the standard deviation that is easy to implement,
-       but definitely not the most efficient. TODO: Make algorithms for std.
-       dev. and mean that use dynamic programming to more efficiently calculate
-       the result if it is run multiple times'''
+    '''Implemented a new method for calculating the mean that stores previous
+    results so that multiple calls are more efficient. Find that using the 
+    numpy method to calculate the standard deviation is more efficient than
+    trying to calculate the standard deviation manually. Storing the results in
+    a dictionary also only marginally improves the speed for large N'''
     new_list = self.rand_list[:cur_index+1]
-    ind_std = numpy.std(new_list)
     ind_mean = self.get_mean(cur_index)
+    ind_std=0
+    if(not(cur_index in self.std_dev_dict)):
+      ind_std = numpy.std(new_list)
+    else:
+      ind_std = self.std_dev_dict[cur_index]
+    print cur_index,ind_mean,ind_std
     return close_val,cur_index,ind_mean,ind_std           
 
 '''Implement the efficient algorithm for calculating the value and index of
 the entry in the n_iters long list of random numbers that is closest to 
-cur_stat. Method for calculating mean and standard deviation is currently 
-not very efficent. Plan to make a dynamic programming algorithm to make the
-std dev and mean calculations more efficient later '''
+cur_stat. Then calculate the mean and standard deviation up to the index.''' 
 def main():
   n_iters = int(sys.argv[1])
   my_rand_list = fizz_rnd_list(n_iters)
@@ -107,6 +129,15 @@ def test_get_mean_zero():
   mean = test_list.get_mean(1)
   assert mean==test_rand[0]
 
+'''Test that if I create a list with one entry that the standard deviation is 
+zero.'''
+def test_get_std_dev_zero():
+  test_list = fizz_rnd_list(1)
+  test_rand = test_list.rand_list
+  mean = test_list.get_mean(1)
+  std_dev = test_list.get_std_dev(1)
+  assert std_dev==0
+
 '''Test that if I create a list with two entries that the mean is the sum of 
 those entries divided by two'''
 def test_get_mean_one():
@@ -115,12 +146,36 @@ def test_get_mean_one():
   mean = test_list.get_mean(2)
   assert mean==((test_rand[0]+test_rand[1])/2)
 
+'''Test that if I create a list with two entries that the standard deviation is
+the square root of the difference of those two entries from their mean divided 
+by two.'''
+def test_get_std_dev_one():
+  test_list = fizz_rnd_list(2)
+  test_rand = test_list.rand_list
+  mean = test_list.get_mean(2)
+  std_dev = test_list.get_std_dev(2)
+  assert std_dev==numpy.sqrt((((test_rand[0]-mean)*(test_rand[0]-mean))/2)+(((test_rand[1]-mean)*(test_rand[1]-mean))/2))
+
 '''Test that if I create a list with three entries that the mean is the sum of 
 those entries divided by three'''
 def test_get_mean_two():
   test_list = fizz_rnd_list(3)
   test_rand = test_list.rand_list
   mean = test_list.get_mean(3)
+
+  assert mean==((test_rand[0]+test_rand[1]+test_rand[2])/3)
+
+'''Test that if I create a list with three entries that the standard deviation 
+is the square root of the difference of those two entries from their mean 
+divided by three.'''
+
+def test_get_std_dev_two():
+  test_list = fizz_rnd_list(3)
+  test_rand = test_list.rand_list
+  mean = test_list.get_mean(3)
+  std_dev = test_list.get_std_dev(3)
+
+  assert std_dev==numpy.sqrt((((test_rand[0]-mean)*(test_rand[0]-mean))/3)+(((test_rand[1]-mean)*(test_rand[1]-mean))/3)+(((test_rand[2]-mean)*(test_rand[2]-mean))/3))
 
 '''Test that if I calculate the mean up to index 1, and then calculate the mean 
 up to index two, that the result is the same as if I calculate the mean just
