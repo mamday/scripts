f0492aaedf550ea61ac21fa17d6e77a7350881de
Melanie Day <mamday@gmail.com>
2016-11-10 17:45:19 -0800

    Implement all the tests except the test for get_stats()

--- get_rand_stats.cxx
+++ get_rand_stats.cxx
@@ -215,7 +215,8 @@ test_rnd_list::test_rnd_list(){
 test_rnd_list::~test_rnd_list(){
 }
 
-//Test initiation of class
+//Test that initiation of fizz_rnd_list class produces 10 entries between 0 and
+//1
 void test_rnd_list::test_fizz_rnd_list_initiation(){
   float l_len = 10;
   fizz_rnd_list test_list(l_len);
@@ -227,6 +228,8 @@ void test_rnd_list::test_fizz_rnd_list_initiation(){
   }
 }
 
+//Test that initiation of int_rnd_list class produces 10 entries between 0 and 
+//1000
 void test_rnd_list::test_int_rnd_list_initiation(){
   int l_len = 10;
   int_rnd_list test_list(l_len);
@@ -236,6 +239,86 @@ void test_rnd_list::test_int_rnd_list_initiation(){
     assert(test_rand[test_len]>0 && test_rand[test_len]<1000);
     test_len+=1;
   }
+}
+
+//Test that uniform standard deviation algorithm returns the expected value
+//given a uniform distribution
+void test_rnd_list::test_uniform_std_dev(){
+  int l_len=1000;
+  int_rnd_list test_list(l_len);
+  double u_std_dev = test_list.get_uniform_std_dev(300);
+
+  assert((u_std_dev-0.0866)<0.0001);
+}
+
+//Test that if I create a list with one entry that the mean is that entry
+void test_rnd_list::test_get_mean_zero(){
+  int_rnd_list test_list(1);
+  double* test_rand = test_list.get_list();
+  double mean = test_list.get_mean(1);
+  assert(mean==test_rand[0]);
+}
+
+//Test that if I create a list with one entry that the standard deviation is 
+//zero 
+void test_rnd_list::test_get_std_dev_zero(){
+  int_rnd_list test_list(1);
+  double* test_rand = test_list.get_list();
+  double mean = test_list.get_mean(1);
+  double std_dev = test_list.get_std_dev(1);
+  assert(std_dev==0);
+}
+
+//Test that if I create a list with two entries that the mean is half of the sum
+//of those two entries 
+void test_rnd_list::test_get_mean_one(){
+  int_rnd_list test_list(2);
+  double* test_rand = test_list.get_list();
+  double mean = test_list.get_mean(2);
+  assert(mean==((test_rand[0]+test_rand[1])/2));
+}
+
+//Test that if I create a list with two entries that the standard deviation is 
+//the square root of the difference of those two entries from their mean divided//by two 
+void test_rnd_list::test_get_std_dev_one(){
+  int_rnd_list test_list(2);
+  double* test_rand = test_list.get_list();
+  double mean = test_list.get_mean(2);
+  double std_dev = test_list.get_std_dev(2);
+  assert(std_dev==sqrt((((test_rand[0]-mean)*(test_rand[0]-mean))/2)+(((test_rand[1]-mean)*(test_rand[1]-mean))/2))
+);
+}
+
+//Test that if I create a list with three entries that the mean is a third of
+// the sum of those three entries 
+void test_rnd_list::test_get_mean_two(){
+  int_rnd_list test_list(3);
+  double* test_rand = test_list.get_list();
+  double mean = test_list.get_mean(3);
+  assert(mean==((test_rand[0]+test_rand[1]+test_rand[2])/3));
+}
+
+//Test that if I calculate the mean up to index 1, and then calculate the mean 
+//up to index two, that the result is the same as if I calculate the mean just
+//once up to index two
+void test_rnd_list::test_get_mean_repeat(){
+  int_rnd_list test_list(2);
+  double* test_rand = test_list.get_list();
+  double mean = test_list.get_mean(1);
+  double mean1 = test_list.get_mean(2);
+  assert(mean1==((test_rand[0]+test_rand[1])/2));
+}
+
+//Test that if I calculate the mean up to index 1, and then calculate the mean 
+//up to index two, and then calculate the mean up to index 3 that the result is
+//the same as if I calculate the mean just once up to index three
+void test_rnd_list::test_get_mean_threepeat(){
+  int_rnd_list test_list(3);
+  double* test_rand = test_list.get_list();
+  double mean = test_list.get_mean(1);
+  double mean1 = test_list.get_mean(2);
+  double mean2 = test_list.get_mean(3);
+  assert(mean2==((test_rand[0]+test_rand[1]+test_rand[2])/3));
 }
 
 // Get the length of the array and the number of times to iterate from the 
@@ -260,6 +343,14 @@ int main(int argc, char* argv[]){
     test_rnd_list tester;
     tester.test_fizz_rnd_list_initiation();
     tester.test_int_rnd_list_initiation();
+    tester.test_uniform_std_dev();
+    tester.test_get_mean_zero();
+    tester.test_get_std_dev_zero();
+    tester.test_get_mean_one();
+    tester.test_get_std_dev_one();
+    tester.test_get_mean_two();
+    tester.test_get_mean_repeat();
+    tester.test_get_mean_threepeat();
   }
   //Exit if the wrong number of inputs are given
   else if(argc != 3){
